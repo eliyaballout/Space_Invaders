844eb788302935ea5dba97520cda86f6daf82beb
eliya <eliyaballout@gmail.com>
2023-08-09 17:11:03 +0300

    fix

--- javascript/game/playerShip.js
+++ javascript/game/playerShip.js
@@ -7,21 +7,21 @@ playerShipSize = {x: 50, y: 50}
 
 function chooseShip(selectedShip){
     switch (selectedShip) {
-        case "resources/images/myShip1.png":
+        case "/resources/images/myShip1.png":
             document.getElementById("myShip1").style.backgroundColor = "red";
             document.getElementById("myShip2").style.backgroundColor = "#b2b3a1";
             document.getElementById("myShip3").style.backgroundColor = "#b2b3a1";
 
             break;
 
-        case "resources/images/myShip2.png":
+        case "/resources/images/myShip2.png":
             document.getElementById("myShip1").style.backgroundColor = "#b2b3a1";
             document.getElementById("myShip2").style.backgroundColor = "red";
             document.getElementById("myShip3").style.backgroundColor = "#b2b3a1";
 
             break;
 
-        case "resources/images/myShip3.png":
+        case "/resources/images/myShip3.png":
             document.getElementById("myShip1").style.backgroundColor = "#b2b3a1";
             document.getElementById("myShip2").style.backgroundColor = "#b2b3a1";
             document.getElementById("myShip3").style.backgroundColor = "red";
